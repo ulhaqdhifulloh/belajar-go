9d3349671c938675fb13bb89529625f91d737c42
ulhaqdhifulloh <ulworld5mail@gmail.com>
2026-02-28 12:46:43 +0700

    feat: add data type material

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+var gate = "\n" + strings.Repeat("*", 20) + "\n"
 
 func main() {
 	fmt.Println("--- Aplikasi Belajar Go Dimulai ---")
-
+	fmt.Print(gate)
 	tampilkanHelloWorld()
+	fmt.Print(gate)
 	tampilkanVariabel()
+	fmt.Print(gate)
+	tampilkanTipeData()
+	fmt.Print(gate)
 
 	fmt.Println("--- Program Selesai ---")
 }
--- variables.go
+++ variables.go
@@ -1,13 +1,37 @@
+// Belajar variabel, nilai konstan, output
+
 package main
 
-import "fmt"
+import ("fmt")
+
+var b = 10
+const hours = 24 // unchangable 
+const (
+    nama = "Ulhaq"
+    hobi string = "Scroll Fesnuk"
+)
 
 func tampilkanVariabel() {
     var student1 string = "John"
     student2 := "Jane"
-    x := 2
+    studentId := 2
+    var a bool
+    student2 = "Janetes"
+    var x, y, z int = 10, 15, 20
+    var c, d = 10, "huhu"
+    var A = true
     
     fmt.Println("Nama Siswa 1:", student1)
     fmt.Println("Nama Siswa 2:", student2)
-    fmt.Println("ID Kelas:", x)
+    fmt.Println("ID Kelas:", studentId)
+    fmt.Println(a)
+    fmt.Println(b)
+    fmt.Println(x+y+z+c)
+    fmt.Println(d)
+    fmt.Println(A)
+    fmt.Printf("Dalam satu hari terdapat %v jam", hours)
+    fmt.Printf("Tipe data %v adalah %T", hobi, hobi)
+    fmt.Printf("%t kah?", A)
+    fmt.Print(nama, hobi)
+    fmt.Println(nama, hobi)
 }
